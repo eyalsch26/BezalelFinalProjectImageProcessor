adcc2e887eefa8e0bc8f6c448fe219595f761ed0
eyalsch26 <eyal.schaffer@mail.huji.ac.il>
2022-11-05 16:03:02 +0200

    Starting corner detection.

--- Vectorizer.py
+++ Vectorizer.py
@@ -1,5 +1,13 @@
 import numpy as np
 
 
+HARRIS_W = 5
+
+
 def harris_corner_detector(im):
     im_gradient = np.gradient(im)
+    ix, iy = im_gradient[0], im_gradient[1]
+    ix_square = np.square(ix)
+    iy_square = np.square(iy)
+    ixiy2 = 2.0 * ix * iy
+
